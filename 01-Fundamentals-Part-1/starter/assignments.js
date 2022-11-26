3b4ce03b79bc54f1f398b2946a67f9540ffb55a4
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-11-26 21:23:18 +0200

    lec-11

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Egypt";
+let continent = "Africa";
+let population = "104.3 Mil";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,1 @@
-let country = "Egypt";
-let continent = "Africa";
-let population = "104.3 Mil";
 
-console.log(country);
-console.log(continent);
-console.log(population);
