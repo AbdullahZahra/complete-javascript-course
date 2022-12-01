f18aba2e0282fa35a2a2769d8d14c7d9eff7eeee
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-12-01 22:42:24 +0200

    lec-15

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,27 +1,27 @@
-const country = "Egypt";
-const continent = "Africa";
-let population = 104300000;
+// const country = "Egypt";
+// const continent = "Africa";
+// let population = 104300000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-const isIsland = false;
-const language = "Arabic";
+// const isIsland = false;
+// const language = "Arabic";
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
-console.log(population / 2);
-console.log(++population);
+// console.log(population / 2);
+// console.log(++population);
 
-const populationFinland = 6000000;
-console.log(population > populationFinland);
+// const populationFinland = 6000000;
+// console.log(population > populationFinland);
 
-const averagePopulation = 33000000;
-console.log(population > averagePopulation);
+// const averagePopulation = 33000000;
+// console.log(population > averagePopulation);
 
-const description = "Egypt is in Africa, and its 104 million people speak Arabic";
-console.log(description);
+// const description = "Egypt is in Africa, and its 104 million people speak Arabic";
+// console.log(description);
