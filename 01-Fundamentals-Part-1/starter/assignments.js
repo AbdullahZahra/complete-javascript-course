ffef44a7c650f47f70d5c37de65c8f10bbb86538
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-12-01 22:25:36 +0200

    lec-14

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,6 @@
 const country = "Egypt";
 const continent = "Africa";
-let population = "104.3 Mil";
+let population = 104300000;
 
 console.log(country);
 console.log(continent);
@@ -13,3 +13,15 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+console.log(population / 2);
+console.log(++population);
+
+const populationFinland = 6000000;
+console.log(population > populationFinland);
+
+const averagePopulation = 33000000;
+console.log(population > averagePopulation);
+
+const description = "Egypt is in Africa, and its 104 million people speak Arabic";
+console.log(description);
