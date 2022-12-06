66a193cb52e8e42025119da2b767c4669302cb17
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-12-06 23:14:57 +0200

    lec-20

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,34 +1,34 @@
-const country = "Egypt";
-const continent = "Africa";
-let population = 104300000;
+// const country = "Egypt";
+// const continent = "Africa";
+// let population = 104300000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-const isIsland = false;
-const language = "Arabic";
+// const isIsland = false;
+// const language = "Arabic";
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
-console.log(population / 2);
-console.log(++population);
+// console.log(population / 2);
+// console.log(++population);
 
-const populationFinland = 6000000;
-console.log(population > populationFinland);
+// const populationFinland = 6000000;
+// console.log(population > populationFinland);
 
-const averagePopulation = 33000000;
-console.log(population > averagePopulation);
+// const averagePopulation = 33000000;
+// console.log(population > averagePopulation);
 
-const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`;
-console.log(description);
+// const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`;
+// console.log(description);
 
-if (population > 33000000) {
-    console.log(`${country}'s population is above average.`);
-}
-else {
-    console.log(`${country}'s population is ${33000000 - population} bellow average.`);
-}
+// if (population > 33000000) {
+//     console.log(`${country}'s population is above average.`);
+// }
+// else {
+//     console.log(`${country}'s population is ${33000000 - population} bellow average.`);
+// }
