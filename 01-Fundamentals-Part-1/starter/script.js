8c9233e2b88eabd9f539d7a5c7c466793a2daf42
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-12-10 22:57:53 +0200

    lec-21

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -151,21 +151,46 @@
 
 
 // type conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'));
-console.log(typeof NaN);
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-// type coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / "2");
+// // type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
 
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// 5 flasy values: 0, '', undefinded, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+}
+else {
+    console.log("You should get a job!");
+}
+
+let height = 0;
+console.log(height);
+if (height) {
+    console.log("YAY! Height is defined");
+}
+else {
+    console.log("Height is UNDEFINED");
+}
